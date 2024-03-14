6e7be748f2e3e9638ed63f8530d09b29a59b1617
eng-wilker <w.martins@campus.fct.unl.pt>
2024-03-14 17:21:36 +0000

    Add doLogin method to LoginResource

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResurce.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResurce.java
@@ -2,9 +2,15 @@ package pt.unl.fct.di.apdc.firstwebapp.resources;
 
 import java.util.logging.Logger;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import pt.unl.fct.di.apdc.firstwebapp.util.LoginData;
+
 
 @Path("/login")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
@@ -12,4 +18,12 @@ public class LoginResurce {
     private static final Logger LOG = Logger.getLogger(LoginResurce.class.getName());
     public LoginResurce() {
     } // nothing to be done here
+
+@POST
+@Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+public Response doLogin(LoginData data) {
+    LOG.fine("Attempt to login user: " + data.username);
+    return Response.ok().build();
+}
 }
---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/pt.unl.fct.di.apdc.firstwebapp.uti/LoginData.java
@@ -0,0 +1,15 @@
+package pt.unl.fct.di.apdc.firstwebapp.util;
+
+public class LoginData {
+    String username;
+    String password;
+
+    public LoginData() {
+    }
+
+    public LoginData(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}

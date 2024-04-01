f16b619bab1a68c0d127edff64bab7978c3e42f1
eng-wilker <w.martins@campus.fct.unl.pt>
2024-04-01 17:15:39 +0100

    novo

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResurce.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResurce.java
@@ -5,14 +5,22 @@ import java.util.logging.Logger;
 import com.google.appengine.repackaged.org.apache.commons.codec.digest.DigestUtils;
 import com.google.cloud.Timestamp;
 import com.google.cloud.datastore.*;
+import com.google.cloud.datastore.StructuredQuery.OrderBy;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.checkerframework.checker.units.qual.A;
+
 import com.google.gson.Gson;
 
 import pt.unl.fct.di.apdc.firstwebapp.util.AuthToken;
@@ -32,19 +40,73 @@ public class LoginResurce {
     @POST
     @Path("/")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response doLogin(LoginData data) {
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response doLogin(LoginData data, @Context HttpServletRequest request, @Context HttpHeaders headers) {
         LOG.fine("Login attempt by user: " + data.username);
-        Key userKey = datastore.newKeyFactory().setKind("User").newKey(data.username);
-        Entity user = datastore.get(userKey);
-        if (user == null) {
-            return Response.status(Response.Status.FORBIDDEN).entity("Incorrect username or password.").build();
+        Key userKey = datastore.newKeyFactory().newKey(data.username);
+        Key ctrsKey = datastore.newKeyFactory().addAncestor(PathElement.of("User", data.username)).setKind("UserStats")
+                .newKey("counters");
+        Key logKey = datastore.allocateId(datastore.newKeyFactory().addAncestor(PathElement.of("User", data.username))
+                .setKind("UserLogs").newKey());
+        Transaction txn = datastore.newTransaction();
+        try {
+            Entity user = txn.get(userKey);
+            if (user == null) {
+                LOG.warning("Failed login attempt for username: " + data.username);
+                return Response.status(Response.Status.FORBIDDEN).entity("Incorrect username or password.").build();
+            }
+            Entity stats = txn.get(ctrsKey);
+            if (stats == null) {
+                stats = Entity.newBuilder(ctrsKey).set("user_stats_logins", 0L).set("user_stats_failed", 0L)
+                        .set("user_first_login", Timestamp.now()).set("user_last_login", Timestamp.now()).build();
+            }
+            String hashedPWD = user.getString("user_pwd");
+            if (hashedPWD.equals(DigestUtils.sha512Hex(data.password))) {
+                Entity log = Entity.newBuilder(logKey).set("user_login_ip", request.getRemoteAddr())
+                        .set("user_login_host", request.getRemoteHost())
+                        .set("user_login_latlon",
+                                StringValue.newBuilder(headers.getHeaderString("X-AppEngine-CityLatLong"))
+                                        .setExcludeFromIndexes(true).build())
+                        .set("user_login_country",
+                                headers.getHeaderString("X-AppEngine-Country"))
+                        .set("user_login_date", Timestamp.now()).set("user_login_city",
+                                headers.getHeaderString("X-AppEngine-City"))
+                        .build();
+
+                Entity ustats = Entity.newBuilder(ctrsKey)
+                        .set("user_stats_logins", 1L + stats.getLong("user_stats_logins"))
+                        .set("user_stats_failed", 0L)
+                        .set("user_first_login", stats.getTimestamp("user_first_login"))
+                        .set("user_last_login", Timestamp.now()).build();
+
+                txn.put(user, ustats, log);
+                txn.commit();
+                AuthToken at = new AuthToken(data.username);
+                LOG.info("User " + data.username + " logged in.");
+                return Response.ok(g.toJson(at)).build();
+            } else {
+                Entity ustats = Entity.newBuilder(ctrsKey).set("user_stats_logins", stats.getLong("user_stats_logins"))
+                        .set("user_stats_failed", 1L + stats.getLong("user_stats_failed"))
+                        .set("user_first_login", stats.getTimestamp("user_first_login"))
+                        .set("user_last_login", stats.getTimestamp("user_last_login"))
+                        .set("user_last_attempt", Timestamp.now()).build();
+                txn.put(ustats);
+                txn.commit();
+                LOG.warning("Failed login attempt for username: " + data.username);
+                return Response.status(Response.Status.FORBIDDEN).entity("Incorrect username or password.").build();
+            }
+        } catch (Exception e) {
+            LOG.severe(e.getMessage());
+            txn.rollback();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error performing login.").build();
+
+        } finally {
+            if (txn.isActive()) {
+                txn.rollback();
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error performing login.").build();
+            }
         }
-        if (!user.getString("password").equals(DigestUtils.sha256Hex(data.password))) {
-            return Response.status(Response.Status.FORBIDDEN).entity("Incorrect username or password.").build();
-        }
-        AuthToken at = new AuthToken(data.username);
-        LOG.info("User " + data.username + " logged in.");
-        return Response.ok(g.toJson(at)).build();
+
     }
 
     @GET
@@ -86,6 +148,33 @@ public class LoginResurce {
                 .set("email", data.email).set("name", data.name).set("timestamp", Timestamp.now()).build();
         datastore.put(user);
         return Response.ok().entity("User registered.").build();
+    }
+
+    @POST
+    @Path("/getLast24Login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getLast24Login(LoginData data) {
+        Key userKey = datastore.newKeyFactory().setKind("User").newKey(data.username);
+        Entity user = datastore.get(userKey);
+        if (user == null) {
+            return Response.status(Response.Status.FORBIDDEN).entity("Incorrect username or password.").build();
+        }
+        if (!user.getString("password").equals(DigestUtils.sha256Hex(data.password))) {
+            return Response.status(Response.Status.FORBIDDEN).entity("Incorrect username or password.").build();
+        }
+        Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind("username")
+                .setFilter(
+                        StructuredQuery.PropertyFilter.eq("username", data.username))
+                .build();
+        QueryResults<Entity> logs = datastore.run(query);
+        int count = 0;
+        while (logs.hasNext()) {
+            logs.next();
+            count++;
+        }
+        return Response.ok().entity(g.toJson(count)).build();
+
     }
 
     @POST
